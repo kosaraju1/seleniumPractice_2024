29bb1bcce9f6a357c0cb740baf4938591923cafa
sudheshna <sudheshnakosaraju@gmail.com>
2024-09-19 21:29:34 -0400

    launching applications

--- src/test/java/seleniumbasics/selenium17/launchingApplications.java
+++ src/test/java/seleniumbasics/selenium17/launchingApplications.java
@@ -1,4 +1,20 @@
-void main()
-{
+package seleniumbasics.selenium17;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.testng.annotations.Test;
+public class launchingApplications {
+
+    @Test
+    public void launchinggoogle(){
+        //where path is exactly present
+        System.out.println(System.getProperty("user.dir"));
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//src//test//resources//driver//chromedriver.exe");
+
+        WebDriver driver=new ChromeDriver();
+
+
+
+
+    }
 
 }
